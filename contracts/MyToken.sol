2550f7550ee7617a66125ea9f2a2dcf0f38caa81
Ilija Kosanin <ilija.kosanin@plusplusnt.rs>
2023-10-05 11:54:04 +0200

    First commit

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor() {}
+    constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, initialSupply);
+    }
 }
